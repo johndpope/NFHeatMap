85ae241321bd428b1bf9ef0efa100272062db057
Gabor Nagy Farkas <nfgabor@gmail.com>
2015-04-24 22:04:23 +0200

    Option for showing the generated locations
    Added the option to show the generated NFLocationPoints
    for better debugging and for showcasing. Read and applied
    best practices for commit messages.

--- NFHeatMapExample/NFHeatMapExample/DemoViewController.swift
+++ NFHeatMapExample/NFHeatMapExample/DemoViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 import MapKit
 
-class DemoViewController: UIViewController{
+class DemoViewController: UIViewController, MKMapViewDelegate
+{
     
     var mapView : MKMapView = MKMapView()
     var locationArray : NSArray = NSArray()
+    var annotationArray : NSMutableArray = NSMutableArray()
     var optionsView : UIView = UIView()
-
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -22,14 +24,15 @@ class DemoViewController: UIViewController{
         self.setupMapView()
         self.setupOptionsView()
         self.setupConstraints()
-    
-        locationArray = DemoHelper.sharedInstance.generateRandomLocationArray(50)
+        
+        locationArray = DemoHelper.sharedInstance.generateRandomLocationArray(10000)
     }
     
     func setupMapView()
     {
         mapView.frame = CGRectZero
         mapView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        mapView.delegate = self
         self.view.addSubview(mapView)
     }
     
@@ -45,6 +48,7 @@ class DemoViewController: UIViewController{
         
         var showAnnotationSwitch : UISwitch = UISwitch()
         showAnnotationSwitch.setTranslatesAutoresizingMaskIntoConstraints(false)
+        showAnnotationSwitch.addTarget(self, action: "switchChanged:", forControlEvents: UIControlEvents.ValueChanged)
         
         optionsView.addSubview(showAnnotationLabel)
         optionsView.addSubview(showAnnotationSwitch)
@@ -54,7 +58,7 @@ class DemoViewController: UIViewController{
         var verticalSwitchConstraint = NSLayoutConstraint.constraintsWithVisualFormat("V:|-5-[showAnnotationSwitch]", options: NSLayoutFormatOptions(0), metrics: nil, views: ["showAnnotationSwitch" : showAnnotationSwitch])
         
         var verticalLabelConstraint = NSLayoutConstraint(item: showAnnotationLabel, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: showAnnotationSwitch, attribute: NSLayoutAttribute.CenterY, multiplier: 1, constant: 0)
-
+        
         optionsView.addConstraints(horizontalConstraint)
         optionsView.addConstraints(verticalSwitchConstraint)
         optionsView.addConstraint(verticalLabelConstraint)
@@ -72,10 +76,49 @@ class DemoViewController: UIViewController{
         self.view.addConstraints(horizontalOptionsViewConstraint)
         self.view.addConstraints(verticalConstraint)
     }
-
+    
+    func switchChanged(sender: UISwitch)
+    {
+        switch sender.on
+        {
+        case true:
+            self.showAnnotationsOnMap()
+        case false:
+            self.hideAnnotationsOnMap()
+        default:
+            break
+        }
+    }
+    
+    func showAnnotationsOnMap()
+    {
+        if(self.annotationArray.count == 0)
+        {
+            for index in 0...(self.locationArray.count - 1)
+            {
+                var currentAnnotation = MKPointAnnotation()
+                let currentLocation = self.locationArray[index] as! NFLocationPoint
+                currentAnnotation.coordinate = currentLocation.location
+                self.annotationArray.addObject(currentAnnotation)
+            }
+        }
+        
+        for index in 0...(self.annotationArray.count - 1)
+        {
+            self.mapView.addAnnotation(self.annotationArray[index] as! MKPointAnnotation)
+        }
+        
+        
+    }
+    
+    func hideAnnotationsOnMap()
+    {
+        self.mapView.removeAnnotations(self.mapView.annotations)
+    }
+    
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
     }
-
+    
 }
